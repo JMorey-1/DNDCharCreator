d677a0aea5c09c2c30a0bab3fe4fa25811156024
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2023-12-04 21:04:18 +0000

    Fixed naming issue

--- src/dndcharcreator/DNDCharCreator.java
+++ src/dndcharcreator/DNDCharCreator.java
@@ -4,8 +4,8 @@ public class DNDCharCreator {
 
     public static void main(String[] args) {
    
-        CharacterOptions characterOptions = new CharacterOptions();
-         characterOptions.createCharacter();
+        options charOptions = new options();
+         charOptions.createCharacter();
         
     } 
 }
