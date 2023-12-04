8d8630250922720c2b2dea9c5d363e00951c20c0
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2023-12-04 20:35:23 +0000

    Create options.java

---
+++ src/dndcharcreator/options.java
@@ -0,0 +1,11 @@
+
+package dndcharcreator;
+
+public class options {
+    
+    
+    private String[] races = {"Human", "Elf", "Dwarf", "Halfling", "Orc"};
+    private String[] classes = {"Barbarian", "Bard", "Cleric", "Druid", "Wizard"};
+    private String[] backgrounds = {"Acolyte", "Sage", "Criminal", "Hermit", "Noble"};
+
+}
