f78de62dd95fe754e404d581d9d2e9297a8b529a
JMorey-1 <152331168+JMorey-1@users.noreply.github.com>
2023-12-04 20:44:19 +0000

    Update options.java

--- src/dndcharcreator/options.java
+++ src/dndcharcreator/options.java
@@ -1,5 +1,6 @@
-
 package dndcharcreator;
+
+import java.util.Random;
 
 public class options {
     
@@ -8,4 +9,26 @@ public class options {
     private String[] classes = {"Barbarian", "Bard", "Cleric", "Druid", "Wizard"};
     private String[] backgrounds = {"Acolyte", "Sage", "Criminal", "Hermit", "Noble"};
 
+    
+        public String getRandomRace() {
+         Random random = new Random();
+         int index = random.nextInt(races.length);
+         return races[index];
+    }
+
+        public String getRandomClass() {
+         Random random = new Random();
+         int index = random.nextInt(classes.length);
+         return classes[index];
+    }
+        public String getRandomBackground() {
+         Random random = new Random();
+         int index = random.nextInt(backgrounds.length);
+         return backgrounds[index];
+        }
+
+
 }
+
+
+
